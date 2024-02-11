3d3f4cd1669207069ed0ed2e5edb9f4ef9107539
sourabh696969 <sourabhkumar2462@gmail.com>
2024-02-11 19:34:05 +0530

    update

--- model/workPostModel.js
+++ model/workPostModel.js
@@ -1,23 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const workPost = mongoose.Schema({
+const workPost = mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     work: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
     },
     agent: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Agent',
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Agent",
     },
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('WorkPost', workPost);
+module.exports = mongoose.model("WorkPost", workPost);
--- server.js
+++ server.js
@@ -2,27 +2,18 @@ const express = require('express');
 const connectDB = require('./config/dbConnection');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
-// const multer = require("multer");
+const cors = require('cors');
 
 connectDB();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;  
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/user', require('./routes/userRoutes'));
 app.use('/api/agent', require('./routes/agentRoutes'));
 app.use('/api/others', require('./routes/workPostRoutes'));
 app.use('/images', express.static('upload/images'));
-
-// function errHandler(err, req, res, next) {
-//     if (err instanceof multer.MulterError) {
-//         res.json({
-//             success: 0,
-//             message: err.message
-//         })
-//     }
-// }
-// app.use(errHandler);
 app.use(errorHandler);
 
 app.listen(PORT, () => {

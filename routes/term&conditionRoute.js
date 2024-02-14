80bd28125a48afefb7fbdd9520cdf8562f7f98b3
sourabh696969 <sourabhkumar2462@gmail.com>
2024-02-14 21:30:50 +0530

    update

--- routes/term&conditionRoute.js
+++ routes/term&conditionRoute.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const { createTermCondition, deleteTermCondition, updateTermCondition, getTermConditionById, getTermCondition } = require('../controllers/term&conditionController');
 
 const router = express.Router();
 
+router.post('/create', createTermCondition);
+router.get('/all', getTermCondition);
+router.get('/:id', getTermConditionById);
+router.put('/:id', updateTermCondition);
+router.delete('/:id', deleteTermCondition);

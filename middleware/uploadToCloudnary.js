716764b46e576e96dcb062d30b27726a1622e22a
sourabh696969 <sourabhkumar2462@gmail.com>
2024-03-11 18:44:59 +0530

    update

--- middleware/uploadToCloudnary.js
+++ middleware/uploadToCloudnary.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
+  api_key: process.env.API_KEY_CLOUDiNARY,
   api_secret: process.env.API_SECRET,
 });
 
